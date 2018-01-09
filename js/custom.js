580b32142585273a1d7a6e102c7fb487d1e65acc
Sadat Islam <sadat21@hotmail.ca>
2018-01-08 21:38:53 -0700

    Enhanced the look of the homepage

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,14 @@
+// https://www.youtube.com/watch?v=o6eJ1K_1CKA
 $(document).ready(function() {
   $('.slide-section').click(function(e) {
 
     var linkHref = $(this).attr('href');
+    var headerHeight = $('nav').outerHeight();
+    console.log(headerHeight);
 
     $('html, body').animate({
-      scrollTop: $(linkHref).offset().top
-    });
+      scrollTop: $(linkHref).offset().top - headerHeight
+    }, 1000);
 
     e.preventDefault();
 
